0abeeb5e3b0e458eaeada26e0038f368e5409d15
gangz <gangz2009@gmail.com>
2016-11-04 07:06:39 +0800

    Add Cucumber runner in test cases

---
+++ src/test/java/cucumber/demo/bank/AccountTransferTest.java
@@ -0,0 +1,17 @@
+package cucumber.demo.bank;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+public class AccountTransferTest {
+
+	@Test
+	public void test() {
+	}
+
+}
