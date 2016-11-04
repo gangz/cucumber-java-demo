5e7eec64c3f6f935ea4c22d8ed91c05f2e15a789
gangz <gangz2009@gmail.com>
2016-11-04 13:26:35 +0800

    add demo of before/after for tag level

---
+++ src/test/java/cucumber/demo/bank/hooks/BankTransferSmookHook.java
@@ -0,0 +1,17 @@
+package cucumber.demo.bank.hooks;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
+public class BankTransferSmookHook {
+
+	@Before("@Smoke")
+	public void setup(){
+		System.out.println("Smoke scenarios begin");
+	}
+	
+	@After("@Smoke")
+	public void tearDownSuccessScenario(){
+		System.out.println("Smoke scenarios end");
+	}
+}
